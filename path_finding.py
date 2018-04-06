e07bd15d2ff891a1afc949a3e35047ded88a099f
Chris9606 <dimitr14@uni.coventry.ac.uk>
2018-04-07 00:48:40 +0100

    Need to upgrade teh queue to that is goes for the neighbor closes to the target, not all the neighbors!

--- path_finding.py
+++ path_finding.py
@@ -1,86 +1,80 @@
 import numpy
 from heapq import *
+import time
 
-def distance(a, b):
+def hscore(a, b):  # the distance from one point to another ( in this case from start node to end goal)
     dx, dy = b[0] - a[0], b[1] - a[1]
     return abs(dx) + abs(dy)
 
-def astar(array, start, goal): # array == map // start, goal == typle
-
+def astar(array, start, goal, red, green, in_rackings): # array == map // start, goal == typle
+    print(" GOAL IS : " + str(goal))
+    print("IN_RAVKINGS IS :" + str(in_rackings))
     neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
-
     close_set = set()   # set so it dones't have dublicates
     came_from = {}
-    gscore = {start:0}
-    fscore = {start:distance(start, goal)}  # the distance from onepoint to another
-    oheap = [] #(fscore[start], start)
+    gscore = {start:0} # hscore from starting node
+    fscore = {start:hscore(start, goal)} # Usually used for diagonal movement but not in here (by combining hcost and gcost)
+    oheap = []
 
     heappush(oheap, (fscore[start], start)) # heappush is an ND function 
     
     while oheap:
-
         current = heappop(oheap)[1] # pops the start and maks it current
 
-        if current == goal:  # check if it's reached the goal
+        if current == goal:  # check if it's reached the goal and returns the path
             data = []
             while current in came_from:
                 data.append(current)
-                current = came_from[current]
-            return data
+                current = came_from[current][0]
+                in_rackings = (in_rackings[0], True)
+                
+            return data, in_rackings
 
         close_set.add(current) # add to visited
         for i, j in neighbors:       # Generates all the neighbors
             neighbor = current[0] + i, current[1] + j            
-            tentative_g_score = gscore[current] + distance(current, neighbor) # adds the so far gscore with the one of the neighbor
+            tentative_g_score = gscore[current] + hscore(current, neighbor) # adds the so far gscore with the one of the neighbor for total value of travel
             if 0 <= neighbor[0] < array.shape[0]: # if neiggbor[0] isn't bigger than 0 or bigger than the array size (going out of the map)
-                if 0 <= neighbor[1] < array.shape[1]:   # shape[0] number if embeded lists, and shape[1] items in each list              
+                if 0 <= neighbor[1] < array.shape[1]:   # shape[0] number if embeded lists, and shape[1] number items in each list              
                     if array[neighbor[0]][neighbor[1]] == 1: 
-                        continue
+                        continue # Skip the remaining code of the for lopp
                 else:
-                    # array bound y walls
-                    continue
+                    # array is on y walls
+                    continue # Skip the remaining code of the for lopp
             else:
-                # array bound x walls
+                # array is on x walls
+                continue # Skip the remaining code of the for lopp
+            
+            print(in_rackings)
+            print("cheking out :" + str(neighbor))
+          
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0): # if the location has been explored and if the current tota cost is bigger, skip the update
                 continue
-                
-            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0): # ????
+
+            if (neighbor[0] != in_rackings[0][0]) and (in_rackings[1] == True): # If a new reakicng entry gets checked out and then passed, change in_racking to False
+                in_rackings = (in_rackings[0], False)
+
+
+            if neighbor in red and in_rackings[1] == False: # If outside the racking, can't enter through the red
+                print("Can't enter red at: " + str(neighbor))
                 continue
-               # current g score < g score for neigbor or neighbor not in heap 
-            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
-                came_from[neighbor] = current
+            elif (neighbor in red) and (in_rackings[1] == True) and (neighbor == (in_rackings[0][0], in_rackings[0][1] + 25 )): # Exit the racking
+                print("Exiting red at: " + str(neighbor))
+                in_rackings = (neighbor, False)
+            elif (neighbor in green) and (in_rackings[1] == True) and (in_rackings[0] == neighbor): # If inside the rakcing, can't exit through green
+                print("Can't exit green at: " + str(neighbor))
+                continue
+            elif (neighbor in green and neighbor != in_rackings[0]): # Enter the racking
+                print("Enter green at: " + str(neighbor))
+                print("AAAAAA " + str(neighbor) + " ------- " + str(in_rackings[0]))
+                in_rackings = (neighbor, True)
+
+
+            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]: # Log the neighbor
+                came_from[neighbor] = (current, in_rackings[1])
                 gscore[neighbor] = tentative_g_score
-                fscore[neighbor] = tentative_g_score + distance(neighbor, goal)
+                fscore[neighbor] = tentative_g_score + hscore(neighbor, goal)
                 heappush(oheap, (fscore[neighbor], neighbor))
-    #print("FINISH")           
-    return False
-'''
-#for testing purposes
-nmap1 = numpy.array([
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
-    [1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
-    [1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1],
-    [1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1],
-    [1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1],
-    [1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1],
-    [1,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,0,1],
-    [1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,1],
-    [1,0,1,1,0,1,1,1,1,0,1,1,1,0,0,0,1,0,0],
-    [1,0,0,1,0,0,0,1,0,0,0,0,1,0,1,0,1,0,1],
-    [1,0,0,1,0,0,0,1,0,0,0,0,1,1,1,0,0,0,1],
-    [1,1,0,1,0,1,0,1,0,1,1,0,1,0,0,0,0,0,1],
-    [1,0,0,1,0,1,0,0,0,0,1,0,0,0,0,0,1,1,1],
-    [1,0,0,1,0,1,1,0,0,0,1,0,0,0,1,1,1,0,1],
-    [1,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1]])
-
- # (11, 8), (12, 8), (13, 8), (13, 7), (13, 6), (12, 6), (11, 6), (10, 6), (10, 5), (10, 4), (9, 4), (8, 4), (7, 4), (7, 3), (7, 2), (7, 1), (6, 1), (5, 1), (4, 1), (3, 1), (2, 1)
-
-start = (1,1)
-goal = (11, 8)
-l = len(astar(nmap1, start, goal)) - 1
-l = astar(nmap1, start, goal)
-print(l)
-'''
+          
+    return False, False
 
--- test_file.py
+++ test_file.py
@@ -1,56 +1,75 @@
 
-import pygame
-from pygame.locals import *
-from path_finding import astar
-import numpy
+iimport numpy
+from heapq import *
 import time
-import threading
-from queue import Queue
-from random import *
-'''
-nameWorker = "worker5"
-print ("aaaaa")
-clock.tick(int(nameWorker[6:]))
-print("bbbbb")
 
-    event = threading.Event()
-    lock.acquire()
-    if (start == (1251, 54)):
-        print(workers_queue[0] + " ? " +  name)
-        while (workers_queue[0] != name):
-             event.wait()
-        print("AAAAAAAAAAAAAAAA")
-        workers_queue.pop(0)
-        event.set(1)
-        lock.release()
+def hscore(a, b):  # the distance from one point to another ( in this case from start node to end goal)
+    dx, dy = b[0] - a[0], b[1] - a[1]
+    return abs(dx) + abs(dy)
+
+def astar(array, start, goal, red, green, in_rackings): # array == map // start, goal == typle
+    print(" GOAL IS : " + str(goal))
+    print("IN_RAVKINGS IS :" + str(in_rackings))
+    neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
+    close_set = set()   # set so it dones't have dublicates
+    came_from = {}
+    gscore = {start:0} # hscore from starting node
+    fscore = {start:hscore(start, goal)} # Usually used for diagonal movement but not in here (by combining hcost and gcost)
+    oheap = []
+
+    heappush(oheap, (fscore[start], start)) # heappush is an ND function 
+    
+    while oheap:
+        current = heappop(oheap)[1] # pops the start and maks it current
+
+        if current == goal:  # check if it's reached the goal and returns the path
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current][0]
+
+            return data, in_rackings
+
+        close_set.add(current) # add to visited
+        for i, j in neighbors:       # Generates all the neighbors
+            neighbor = current[0] + i, current[1] + j            
+            tentative_g_score = gscore[current] + hscore(current, neighbor) # adds the so far gscore with the one of the neighbor for total value of travel
+            if 0 <= neighbor[0] < array.shape[0]: # if neiggbor[0] isn't bigger than 0 or bigger than the array size (going out of the map)
+                if 0 <= neighbor[1] < array.shape[1]:   # shape[0] number if embeded lists, and shape[1] number items in each list              
+                    if array[neighbor[0]][neighbor[1]] == 1: 
+                        continue # Skip the remaining code of the for lopp
+                else:
+                    # array is on y walls
+                    continue # Skip the remaining code of the for lopp
+            else:
+                # array is on x walls
+                continue # Skip the remaining code of the for lopp
+            
+            print(in_rackings)
+            print("cheking out :" + str(neighbor))
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0): # if the location has been explored and if the current tota cost is bigger, skip the update
+                continue
+
+            if neighbor in red and in_rackings[1] == False: # If outside the racking, can't enter through the red
+                print("Can't enter red at: " + str(neighbor))
+                continue
+            elif (neighbor in red) and (in_rackings[1] == True) and (in_rackings[0] == neighbor): # Exit the racking
+                print("Exiting red at: " + str(neighbor))
+                in_rackings = (neighbor, False)
+            elif (neighbor in green) and (in_rackings[1] == True) and (in_rackings[0] == neighbor): # If inside the rakcing, can't exit through green
+                print("Can't exit green at: " + str(neighbor))
+                continue
+            elif (neighbor in green and neighbor != in_rackings[0] and in_rackings[1] != True): # Enter the racking
+                print("Enter green at: " + str(neighbor))
+                print("AAAAAA " + str(neighbor) + " ------- " + str(in_rackings[0]))
+                in_rackings = (neighbor, True)
 
 
+            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]: # Log the neighbor
+                came_from[neighbor] = (current, in_rackings[1])
+                gscore[neighbor] = tentative_g_score
+                fscore[neighbor] = tentative_g_score + hscore(neighbor, goal)
+                heappush(oheap, (fscore[neighbor], neighbor))
+          
+    return False, False
 
-
-        alphabet = []                # Generate the alphabet for vertical point
-    for letter in range(97,123):
-        alphabet.append(chr(letter))
-
-        with open('batches', 'wb') as fp:    # Dump the batches genrated last time
-        pickle.dump(batches, fp)
-
-
-
-
-                print("tick " + str(pygame.time.get_ticks()))
-
-
-                import random
-import pickle
-'''
-
-
-
-
-a = [1,2,3,4,5,6,7,8,9]
-
-for number in a:
-    if number == 5:
-        a =[1,2,3,4,5,8,9]
-    print(number)
-                            
